158c54c78c5e9e76770780bda9f696472d1422f2
jpvital <joao.campos18@gmail.com>
2014-03-26 14:21:21 +0000

    created models

--- sperozone/sperozone/settings.py
+++ sperozone/sperozone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
+    'sperozoneApp',
 )
 
 MIDDLEWARE_CLASSES = (
--- sperozone/sperozoneApp/models.py
+++ sperozone/sperozoneApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ocorrencia(models.Model):
+	title = models.CharField(max_length = 100)
+	description = models.CharField(max_length = 200)
+	report_date = models.DateTimeField("Reportado em:")
+	completion_date = models.DateTimeField("Concluido em:")
+	status = models.CharField(max_length = 50)
+
+
+	def __unicode__(self):
+		return self.title
