245baf797ef56d2a9f30a99315ea56fc11abdbb5
jpvital <joao.campos18@gmail.com>
2014-03-26 16:14:29 +0000

    listagem de ocorrencias

--- sperozone/sperozone/urls.py
+++ sperozone/sperozone/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sperozone.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^sperozoneApp/', include('sperozoneApp.urls', namespace="sperozoneApp")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- sperozone/sperozoneApp/views.py
+++ sperozone/sperozoneApp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+from sperozoneApp.models import Ocorrencia
+
+class IndexView(generic.ListView):
+    template_name = 'sperozoneApp/index.html'
+    context_object_name = 'lista_de_ocorrencias'
+
+    def get_queryset(self):
+        return Ocorrencia.objects.all()
+
+
+
+class DetailView(generic.DetailView):
+    model = Ocorrencia
+    template_name = 'sperozoneApp/detail.html'
+
+def index(request):
+	ocorrencias = Ocorrencia.objects.all()
+	context = {'lista_de_ocorrencias':ocorrencias}
+	return render(request,'sperozoneApp/index.html',context)
+
+def detail(request, poll_id):
+	ocorrencia = get_object_or_404(Ocorrencia, pk=ocorrencia_id)
+	return render(request, 'sperozoneApp/detail.html',{'ocorrencia':ocorrencia})
