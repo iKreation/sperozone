a664d37b4f9b07d3df55b58cf7f29434dc52ac09
jpvital <joao.campos18@gmail.com>
2014-03-30 19:08:43 +0100

    correçoes ao backend

--- sperozone/sperozoneApp/urls.py
+++ sperozone/sperozoneApp/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import patterns, url
 from sperozoneApp import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/$', views.controller, name='controller'),
 )
--- sperozone/sperozoneApp/views.py
+++ sperozone/sperozoneApp/views.py
@@ -1,61 +1,45 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
-import simplejsons
+import json
 
 from sperozoneApp.models import Ocorrencia
 
-class IndexView(generic.ListView):
-    template_name = 'sperozoneApp/index.html'
-    context_object_name = 'lista_de_ocorrencias'
-
-    def get_queryset(self):
-        return Ocorrencia.objects.all()
-
-
-class DetailView(generic.DetailView):
-    model = Ocorrencia
-    template_name = 'sperozoneApp/detail.html'
-
-def index(request):
-	ocorrencias = Ocorrencia.objects.all()
-	context = {'lista_de_ocorrencias':ocorrencias}
-	return render(request,'sperozoneApp/index.html',context)
-
 def get_ocorrencia(request, ocorrencia_id):
 	ocorrencia = get_object_or_404(Ocorrencia, pk=ocorrencia_id)
-	return render(request, 'sperozoneApp/detail.html',{'ocorrencia':ocorrencia})
+	return HttpResponse("A aceder a ocorrencia")
 
 def remove_ocorrencia(oc_id):
-    return Ocorrencia.objects.filter(id=oc_id).delete()
+    Ocorrencia.objects.filter(id=oc_id).delete()
+    return HttpResponse("Ocorrencia eliminada")
 
 def edit_ocorrencia(request, oc_id):
     ocorrencia = Ocorrencia.objectos.filter(id=oc_id)
-    data = simplejson.loads(request.body)
+    data = json.loads(request.body)
     ocorrencia.title = data['title']
     ocorrencia.description = data['description']
     ocorrencia.report_date = timezone.now()
     ocorrencia.status = data['title']
     ocorrencia.save()
-    return
+    return HttpResponse("Ocorrencia modificada")
 
 def new_ocorrencia(request):
-    data = simplejson.loads(request.body)
+    data = json.loads(request.body)
 
     ocorrencia = Ocorrencia(title=data['title'],description=data['description'],report_date=timezone.now(),status=data['status'])
     ocorrencia.save()
-    return
+    return HttpResponse("Nova ocorrencia adicionada")
 
-def controller(request, oc_id):
+def controller(request, pk):
     if request.method == 'DELETE':
-        return remove_ocorrencia(oc_id)
+        return remove_ocorrencia(pk)
     elif request.method == 'GET':
-        return get_ocorrencia(request,oc_id)
+        return get_ocorrencia(request,pk)
     elif request.method == 'PUT' or 'POST':
-        data = simplejson.loads(request.body)
+        data = json.loads(request.body)
         if data.has_key('id') and data['id'] == 0:
             return new_ocorrencia(request)
         else:
-            return edit_ocorrencia(request,oc_id)
+            return edit_ocorrencia(request,pk_id)
