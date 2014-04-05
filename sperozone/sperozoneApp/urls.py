171dfa832e73a84a56aa1bdb84793108c70f1f24
jpvital <joao.campos18@gmail.com>
2014-04-05 16:46:27 +0100

    git conflicts #2

--- sperozone/sperozoneApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from sperozoneApp import views
-
-urlpatterns = patterns('',
-    url(r'^ocorrencias/(?P<pk>\d+)', views.controller, name='controller'),
-    url(r'^ocorrencias/',views.controller, name='controller'),
-)
